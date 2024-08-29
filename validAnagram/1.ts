df49b10101515cf5e8d40adddb05b844ee6d3ea9
ReushevKirill <reushev.kd@gmail.com>
2024-08-29 10:46:59 +0300

    Improved the solution

--- validAnagram/1.ts
+++ validAnagram/1.ts
@@ -1,25 +1,18 @@
 function isAnagram(s: string, t: string): boolean {
+	let hashMapS = new Map()
+	let hashMapT = new Map()
+
 	if (s.length !== t.length) return false
 
-	const countS: Map<string, number> = new Map()
-	const countT: Map<string, number> = new Map()
-
 	for (let i = 0; i < s.length; i++) {
-		if (countS.get(s[i])) {
-			countS.set(s[i], 1 + countS.get(s[i]))
-		} else {
-			countS.set(s[i], 1)
-		}
-
-		if (countT.get(t[i])) {
-			countT.set(t[i], 1 + countT.get(t[i]))
-		} else {
-			countT.set(t[i], 1)
-		}
+		hashMapS.set(s[i], hashMapS.has(s[i]) ? hashMapS.get(s[i]) + 1 : 1)
+		hashMapT.set(t[i], hashMapT.has(t[i]) ? hashMapT.get(t[i]) + 1 : 1)
 	}
 
-	for (let j = 0; j <= countS.size; j++) {
-		if (countS.get(s[j]) !== countT.get(s[j])) return false
+	if (hashMapS.size !== hashMapT.size) return false
+
+	for (let j = 0; j <= hashMapS.size; j++) {
+		if (hashMapS.get(s[j]) !== hashMapT.get(s[j])) return false
 	}
 
 	return true
